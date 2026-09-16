6b79024e6d297d183143151f0d108ae757a1ab98
Gary Kedziora <kedziora@derecho3.hsn.de.hpc.ucar.edu>
2026-09-16 14:59:12 -0600

    Used relative path to repository launch_cf

--- PBS/job_arrays/command_files/examples/derecho-640procs/submit_launch_cf.sh
+++ PBS/job_arrays/command_files/examples/derecho-640procs/submit_launch_cf.sh
@@ -2,4 +2,4 @@
 
 export OMP_NUM_THREADS=1
 
-launch_cf -A $PBS_ACCOUNT -l walltime=00:10:00 ./cmdfile |& tee launch_cf.log
+../../launch_cf -A $PBS_ACCOUNT -l walltime=00:10:00 ./cmdfile |& tee launch_cf.log
